d2cd22ac5b33163f63ae8f715cb8f0c698a48ba4
JAICHANGPARK <aristojeff@gmail.com>
2025-07-10 18:23:48 +0900

    Update ep2301_main.dart

---
+++ lib/ep2301_barber_booking_app/ep2301_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class BarberBookingApp extends StatelessWidget {
+  const BarberBookingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
