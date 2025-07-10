8fe88de5698a16890a4d600acbf829069e5e23a3
JAICHANGPARK <aristojeff@gmail.com>
2025-07-10 18:27:47 +0900

    Update ep2301_main.dart

--- lib/ep2301_barber_booking_app/ep2301_main.dart
+++ lib/ep2301_barber_booking_app/ep2301_main.dart
@@ -4,11 +4,3 @@ void main() {
   runApp(BarberBookingApp());
 }
 
-class BarberBookingApp extends StatelessWidget {
-  const BarberBookingApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp();
-  }
-}
