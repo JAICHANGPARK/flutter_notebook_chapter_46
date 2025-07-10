6d493a1ed19eb0f49e81af7092fe4a0e5441630c
JAICHANGPARK <aristojeff@gmail.com>
2025-07-10 18:28:15 +0900

    Update barber_booking_home_screen.dart

---
+++ lib/ep2301_barber_booking_app/home/ui/barber_booking_home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BarberBookingHomeScreen extends StatefulWidget {
+  const BarberBookingHomeScreen({super.key});
+
+  @override
+  State<BarberBookingHomeScreen> createState() => _BarberBookingHomeScreenState();
+}
+
+class _BarberBookingHomeScreenState extends State<BarberBookingHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
