00725ef2049ee453b509cdf74916b9a615bf3528
JAICHANGPARK <aristojeff@gmail.com>
2025-07-14 17:09:13 +0900

    Update food_delivery_app.dart

--- lib/ep2305_food_delivery_app/food_delivery_app.dart
+++ lib/ep2305_food_delivery_app/food_delivery_app.dart
@@ -6,6 +6,8 @@ class FoodDeliveryApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      
+    );
   }
 }
