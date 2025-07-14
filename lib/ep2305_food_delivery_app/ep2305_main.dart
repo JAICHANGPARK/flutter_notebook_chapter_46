7c3ee360eadc1f3b27c2e459cb65e1049cc0298c
JAICHANGPARK <aristojeff@gmail.com>
2025-07-14 17:09:26 +0900

    Update ep2305_main.dart

--- lib/ep2305_food_delivery_app/ep2305_main.dart
+++ lib/ep2305_food_delivery_app/ep2305_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_46/ep2305_food_delivery_app/food_delivery_app.dart';
 
 
 void main() {
-  
+  runApp(FoodDeliveryApp());
 }
