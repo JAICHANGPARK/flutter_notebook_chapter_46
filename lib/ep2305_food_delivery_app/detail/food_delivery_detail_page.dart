3af692d7a3587deeea3d9f8724363eb5749ce413
JAICHANGPARK <aristojeff@gmail.com>
2025-07-17 19:02:45 +0900

    Update food_delivery_detail_page.dart

--- lib/ep2305_food_delivery_app/detail/food_delivery_detail_page.dart
+++ lib/ep2305_food_delivery_app/detail/food_delivery_detail_page.dart
@@ -119,10 +119,15 @@ class _FoodDeliveryDetailPageState extends State<FoodDeliveryDetailPage> {
                       CircleAvatar(radius: 26),
                     ],
                   ),
-                  Text("Description",style: TextStyle(),),
+                  Text(
+                    "Description",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
                   ReadMoreText(
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     trimLines: 3,
+                    style: TextStyle(color: Colors.grey),
+                    
                   ),
                   Row(
                     children: [
