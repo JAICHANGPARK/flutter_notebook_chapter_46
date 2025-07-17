0f1b64f58a5a1a673ce3c0c3874dac11d985dc1b
JAICHANGPARK <aristojeff@gmail.com>
2025-07-17 19:16:48 +0900

    Update food_delivery_cart_page.dart

---
+++ lib/ep2305_food_delivery_app/ui/cart/food_delivery_cart_page.dart
@@ -0,0 +1,13 @@
+class FoodDeliveryCartPage extends StatefulWidget {
+  const FoodDeliveryCartPage({super.key});
+
+  @override
+  State<FoodDeliveryCartPage> createState() => _FoodDeliveryCartPageState();
+}
+
+class _FoodDeliveryCartPageState extends State<FoodDeliveryCartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
