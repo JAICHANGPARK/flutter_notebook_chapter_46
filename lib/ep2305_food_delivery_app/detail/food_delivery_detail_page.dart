b40ba459c5fb2f01faa52fd567e36043497b8944
JAICHANGPARK <aristojeff@gmail.com>
2025-07-17 18:38:45 +0900

    Update food_delivery_detail_page.dart

---
+++ lib/ep2305_food_delivery_app/detail/food_delivery_detail_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FoodDeliveryDetailPage extends StatefulWidget {
+  const FoodDeliveryDetailPage({super.key});
+
+  @override
+  State<FoodDeliveryDetailPage> createState() => _FoodDeliveryDetailPageState();
+}
+
+class _FoodDeliveryDetailPageState extends State<FoodDeliveryDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+
+      ),
+    );
+  }
+}
