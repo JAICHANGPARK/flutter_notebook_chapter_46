cf575de22df2994deabd6af051eae1fa8713b370
JAICHANGPARK <aristojeff@gmail.com>
2025-07-23 18:38:29 +0900

    Update ep2314_main.dart

--- lib/ep2314_sport_mobile_app/ep2314_main.dart
+++ lib/ep2314_sport_mobile_app/ep2314_main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_46/ep2314_sport_mobile_app/sport_mobile_app.dart';
 import 'package:flutter_notebook_chapter_46/ep2314_sport_mobile_app/ui/sport_mobile_home_page.dart';
 
 void main() {
-  runApp(SportMobileHomePage());
+  runApp(SportMobileApp());
 }
 
