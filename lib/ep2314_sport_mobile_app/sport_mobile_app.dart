3d5ffadff8c2472d1bd3aa5597991526c879211d
JAICHANGPARK <aristojeff@gmail.com>
2025-07-23 18:35:45 +0900

    Update sport_mobile_app.dart

--- lib/ep2314_sport_mobile_app/sport_mobile_app.dart
+++ lib/ep2314_sport_mobile_app/sport_mobile_app.dart
@@ -6,6 +6,8 @@ class SportMobileApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: ,
+    );
   }
 }
