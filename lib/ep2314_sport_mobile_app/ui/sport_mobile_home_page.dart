bfd22b05a0f2ec906230ddb3af5d6f986797edfd
JAICHANGPARK <aristojeff@gmail.com>
2025-07-23 18:35:59 +0900

    Update sport_mobile_home_page.dart

--- lib/ep2314_sport_mobile_app/ui/sport_mobile_home_page.dart
+++ lib/ep2314_sport_mobile_app/ui/sport_mobile_home_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class SportMobileHomePage extends StatefulWidget {
+  const SportMobileHomePage({super.key});
+
+  @override
+  State<SportMobileHomePage> createState() => _SportMobileHomePageState();
+}
+
+class _SportMobileHomePageState extends State<SportMobileHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
