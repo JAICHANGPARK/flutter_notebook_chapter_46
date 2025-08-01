c6ab5f12b0d629a6ba0ef45ac4cb049e4fae29fd
JAICHANGPARK <aristojeff@gmail.com>
2025-08-01 17:34:16 +0900

    Update ep2323_main.dart

--- lib/ep2323_taxi_booking_app/ep2323_main.dart
+++ lib/ep2323_taxi_booking_app/ep2323_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_46/ep2323_taxi_booking_app/taxi_booking_app.dart';
 
 void main() {
-  
+  runApp(TaxiBookingApp());
 }
