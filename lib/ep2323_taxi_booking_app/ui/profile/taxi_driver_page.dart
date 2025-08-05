79dbc68cd5dff0a55b27d6c3ee196f0259f0f883
JAICHANGPARK <aristojeff@gmail.com>
2025-08-05 18:34:47 +0900

    Create taxi_driver_page.dart

---
+++ lib/ep2323_taxi_booking_app/ui/profile/taxi_driver_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TaxiDriverPage extends StatefulWidget {
+  const TaxiDriverPage({super.key});
+
+  @override
+  State<TaxiDriverPage> createState() => _TaxiDriverPageState();
+}
+
+class _TaxiDriverPageState extends State<TaxiDriverPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
