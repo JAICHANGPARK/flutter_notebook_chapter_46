3eea8d4ce01ce054858cf02a11631eccd4152d22
JAICHANGPARK <aristojeff@gmail.com>
2025-08-08 17:23:11 +0900

    Update interior_shopping_app.dart

--- lib/ep2330_interior_shopping_app/interior_shopping_app.dart
+++ lib/ep2330_interior_shopping_app/interior_shopping_app.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
-
 class InteriorShoppingApp extends StatelessWidget {
   const InteriorShoppingApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: Color.fromRGBO(236, 232, 229, 1),
+      ),
     );
   }
 }
