f8ed5f0926dde4df75799bce2c7b943bbb6b05b8
JAICHANGPARK <aristojeff@gmail.com>
2025-08-08 17:26:50 +0900

    Update interior_explore_page.dart

--- lib/ep2330_interior_shopping_app/ui/interior_explore_page.dart
+++ lib/ep2330_interior_shopping_app/ui/interior_explore_page.dart
@@ -12,35 +12,38 @@ class _InteriorExplorePageState extends State<InteriorExplorePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                CircleAvatar(radius: 32, backgroundColor: Colors.white),
-                Container(
-                  height: 64,
-                  width: 64,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.white,
-                  ),
-                  child: Stack(
-                    children: [
-                      CircleAvatar(radius: 32, backgroundColor: Colors.white),
-                      Positioned(
-                        top: 2,
-                        right: 2,
-                        child: CircleAvatar(
-                          radius: 6,
-                          backgroundColor: Colors.red,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(radius: 32, backgroundColor: Colors.white),
+                  Container(
+                    height: 64,
+                    width: 64,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                    child: Stack(
+                      children: [
+                        CircleAvatar(radius: 32, backgroundColor: Colors.white),
+                        Positioned(
+                          top: 2,
+                          right: 2,
+                          child: CircleAvatar(
+                            radius: 6,
+                            backgroundColor: Colors.red,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
