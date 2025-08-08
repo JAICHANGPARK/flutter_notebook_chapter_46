7d11c94ab973969775b78ecc617aad153d14a484
JAICHANGPARK <aristojeff@gmail.com>
2025-08-08 17:22:16 +0900

    Create interior_explore_page.dart

---
+++ lib/ep2330_interior_shopping_app/ui/interior_explore_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InteriorExplorePage extends StatefulWidget {
+  const InteriorExplorePage({super.key});
+
+  @override
+  State<InteriorExplorePage> createState() => _InteriorExplorePageState();
+}
+
+class _InteriorExplorePageState extends State<InteriorExplorePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
