1a6f4e72990f21fac573885763a4beeb710b351c
JAICHANGPARK <aristojeff@gmail.com>
2025-08-08 17:21:11 +0900

    Update ep2330_main.dart

--- lib/ep2330_interior_shopping_app/ep2330_main.dart
+++ lib/ep2330_interior_shopping_app/ep2330_main.dart
@@ -1,8 +1,6 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_notebook_chapter_46/ep2330_interior_shopping_app/interior_shopping_app.dart';
 
 void main() {
-runApp(InteriorShoppingApp());
+  runApp(InteriorShoppingApp());
 }
