5ae2f8b6f74e4009b03ce9b53028c261a080e624
JAICHANGPARK <aristojeff@gmail.com>
2025-08-10 08:04:09 +0900

    Update interior_cart_page.dart

--- lib/ep2330_interior_shopping_app/ui/interior_cart_page.dart
+++ lib/ep2330_interior_shopping_app/ui/interior_cart_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class InteriorCartPage extends StatefulWidget {
+  const InteriorCartPage({super.key});
+
+  @override
+  State<InteriorCartPage> createState() => _InteriorCartPageState();
+}
+
+class _InteriorCartPageState extends State<InteriorCartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
