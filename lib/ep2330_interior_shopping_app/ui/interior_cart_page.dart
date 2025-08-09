b683a5d383b93376554ab4ab5c91355776562a5b
JAICHANGPARK <aristojeff@gmail.com>
2025-08-10 08:04:24 +0900

    Update interior_cart_page.dart

--- lib/ep2330_interior_shopping_app/ui/interior_cart_page.dart
+++ lib/ep2330_interior_shopping_app/ui/interior_cart_page.dart
@@ -10,6 +10,8 @@ class InteriorCartPage extends StatefulWidget {
 class _InteriorCartPageState extends State<InteriorCartPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(child: Column()),
+    );
   }
 }
