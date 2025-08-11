9233c194ada42969653237b08b48e9ee407333a5
JAICHANGPARK <aristojeff@gmail.com>
2025-08-11 20:18:51 +0900

    Update e_commerce_main_page.dart

--- lib/ep2335_e_commerce_app/ui/e_commerce_main_page.dart
+++ lib/ep2335_e_commerce_app/ui/e_commerce_main_page.dart
@@ -14,55 +14,108 @@ class _ECommerceMainPageState extends State<ECommerceMainPage> {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned.fill(child: SafeArea(child: Column())),
-          Align(
-            child: Container(
-              child: Row(
-                spacing: 6,
+          Positioned.fill(
+            child: SafeArea(
+              child: Column(
                 children: [
-                  CircleAvatar(
-                    radius: 42,
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black,
-                    child: Icon(HugeIcons.strokeRoundedHome03, size: 32),
-                  ),
-                  Container(
-                    height: 84,
-                    width: 84,
-                    decoration: BoxDecoration(),
-                    child: Stack(
-                      children: [
-                        CircleAvatar(
-                          radius: 46,
-                          backgroundColor: Colors.black,
-                          child: Icon(
-                            HugeIcons.strokeRoundedShoppingBasket03,
-                            size: 32,
-                          ),
-                          foregroundColor: Colors.white,
+                  Row(
+                    children: [
+                      CircleAvatar(),
+                      Expanded(
+                        child: Column(
+                          children: [Text("Hi Dream!"), Text("Welcome back")],
                         ),
-                        Positioned(
-                          right: 0,
-                          top: 0,
-                          child: CircleAvatar(
-                            radius: 12,
-                            backgroundColor: Colors.grey,
-                            child: CircleAvatar(
-                              radius: 6,
-                              backgroundColor: Colors.red,
+                      ),
+                      CircleAvatar(),
+                      Container(
+                        height: 42,
+                        width: 42,
+                        decoration: BoxDecoration(),
+                        child: Stack(
+                          children: [
+                            CircleAvatar(
+                              radius: 26,
+                              backgroundColor: Colors.black,
+                              child: Icon(
+                                HugeIcons.strokeRoundedShoppingBasket03,
+                                size: 32,
+                              ),
+                              foregroundColor: Colors.white,
                             ),
-                          ),
+                            Positioned(
+                              right: 0,
+                              top: 0,
+                              child: CircleAvatar(
+                                radius: 12,
+                                backgroundColor: Colors.grey,
+                                child: CircleAvatar(
+                                  radius: 6,
+                                  backgroundColor: Colors.red,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
-                  CircleAvatar(
-                    radius: 42,
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black,
-                    child: Icon(HugeIcons.strokeRoundedUser, size: 32),
+                      ),
+                    ],
                   ),
                 ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 32),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 6,
+                  children: [
+                    CircleAvatar(
+                      radius: 42,
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                      child: Icon(HugeIcons.strokeRoundedHome03, size: 32),
+                    ),
+                    Container(
+                      height: 84,
+                      width: 84,
+                      decoration: BoxDecoration(),
+                      child: Stack(
+                        children: [
+                          CircleAvatar(
+                            radius: 46,
+                            backgroundColor: Colors.black,
+                            child: Icon(
+                              HugeIcons.strokeRoundedShoppingBasket03,
+                              size: 32,
+                            ),
+                            foregroundColor: Colors.white,
+                          ),
+                          Positioned(
+                            right: 0,
+                            top: 0,
+                            child: CircleAvatar(
+                              radius: 12,
+                              backgroundColor: Colors.grey,
+                              child: CircleAvatar(
+                                radius: 6,
+                                backgroundColor: Colors.red,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 42,
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                      child: Icon(HugeIcons.strokeRoundedUser, size: 32),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
