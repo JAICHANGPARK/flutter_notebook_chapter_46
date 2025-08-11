5996b13d6430a09bcbb10620ecad2682c6df54f5
JAICHANGPARK <aristojeff@gmail.com>
2025-08-11 18:47:29 +0900

    Update furniture_cart.dart

--- lib/ep2330_interior_shopping_app/model/furniture_cart.dart
+++ lib/ep2330_interior_shopping_app/model/furniture_cart.dart
@@ -1,7 +1,15 @@
+import 'dart:ui';
+
 import 'furniture.dart';
 
 class FurnitureCart extends Furniture {
   int? count;
 
-  FurnitureCart({this.count});
+  FurnitureCart({
+    this.count,
+    super.title,
+    super.price,
+    super.rating,
+    super.backgroundColor,
+  });
 }
