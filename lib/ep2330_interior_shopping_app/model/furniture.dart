a79cb50757911cacb6dc3de86b70d985edff1a12
JAICHANGPARK <aristojeff@gmail.com>
2025-08-11 18:48:26 +0900

    Update furniture.dart

--- lib/ep2330_interior_shopping_app/model/furniture.dart
+++ lib/ep2330_interior_shopping_app/model/furniture.dart
@@ -3,9 +3,16 @@ import 'dart:ui';
 class Furniture {
   String? title;
   String? price;
+  String? category;
   int? rating;
   Color? backgroundColor;
 
   ///windows Alt + Insert
-  Furniture({this.title, this.price, this.rating, this.backgroundColor});
+  Furniture({
+    this.title,
+    this.price,
+    this.rating,
+    this.category = "Chair",
+    this.backgroundColor,
+  });
 }
