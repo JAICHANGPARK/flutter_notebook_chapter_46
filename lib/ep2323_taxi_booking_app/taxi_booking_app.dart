14299aa8f78231d8272961896f5e26ec5a9bbb69
Dreamwalker <aristojeff@gmail.com>
2025-08-13 14:44:24 +0800

    Update taxi_booking_app.dart

--- lib/ep2323_taxi_booking_app/taxi_booking_app.dart
+++ lib/ep2323_taxi_booking_app/taxi_booking_app.dart
@@ -2,14 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'ui/home/taxi_booking_home_page.dart';
 
-
 class TaxiBookingApp extends StatelessWidget {
   const TaxiBookingApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: TaxiBookingHomePage(),
-    );
+    return MaterialApp(home: TaxiBookingHomePage());
   }
 }
