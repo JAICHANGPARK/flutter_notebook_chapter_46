e2dbe970bfd54a58e530a8140cdbd6953b9142a3
Dreamwalker <aristojeff@gmail.com>
2025-08-14 06:15:09 +0800

    Update sport_mobile_app.dart

--- lib/ep2314_sport_mobile_app/sport_mobile_app.dart
+++ lib/ep2314_sport_mobile_app/sport_mobile_app.dart
@@ -2,14 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'ui/sport_mobile_home_page.dart';
 
-
 class SportMobileApp extends StatelessWidget {
   const SportMobileApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SportMobileHomePage(),
-    );
+    return MaterialApp(home: SportMobileHomePage());
   }
 }
