6b5efcea3ea70ed50e27351f8402afd9835d553c
Dreamwalker <aristojeff@gmail.com>
2025-08-15 09:12:51 +0800

    Update ep2305_main.dart

--- lib/ep2305_food_delivery_app/ep2305_main.dart
+++ lib/ep2305_food_delivery_app/ep2305_main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_46/ep2305_food_delivery_app/food_delivery_app.dart';
 
-
 void main() {
   runApp(FoodDeliveryApp());
 }
