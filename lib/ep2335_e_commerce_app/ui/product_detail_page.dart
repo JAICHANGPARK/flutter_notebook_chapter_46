25d919d8f109d86b653c423265bc55a1de481e4d
JAICHANGPARK <aristojeff@gmail.com>
2025-08-18 16:48:19 +0900

    Update product_detail_page.dart

--- lib/ep2335_e_commerce_app/ui/product_detail_page.dart
+++ lib/ep2335_e_commerce_app/ui/product_detail_page.dart
@@ -11,6 +11,12 @@ class ProductDetailPage extends StatefulWidget {
 class _ProductDetailPageState extends State<ProductDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
   }
 }
