a2c3f7c196c6ae1b85fbe43e8bade30c4176440d
JAICHANGPARK <aristojeff@gmail.com>
2025-08-18 16:55:20 +0900

    Update product_detail_page.dart

--- lib/ep2335_e_commerce_app/ui/product_detail_page.dart
+++ lib/ep2335_e_commerce_app/ui/product_detail_page.dart
@@ -13,21 +13,34 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned.fill(bottom: 240, child: Placeholder()),
+          Positioned.fill(
+            bottom: 240,
+            child: PageView(
+              children: [
+                Image.network(
+                  "https://cdn.pixabay.com/photo/2019/08/07/07/05/woman-4390055_1280.jpg",
+                  fit: BoxFit.cover,
+                ),
+                Image.network(
+                  "https://cdn.pixabay.com/photo/2022/06/21/23/11/asian-7276658_1280.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ],
+            ),
+          ),
           Positioned(
             bottom: 0,
             left: 0,
             right: 0,
-            child: Container(height: 320, child: Stack(
-              children: [
-                Positioned(child: Placeholder()),
-                Align(child: Row(
-                  children: [
-                    
-                  ],
-                ),)
-              ],
-            )),
+            child: Container(
+              height: 320,
+              child: Stack(
+                children: [
+                  Positioned(child: Placeholder()),
+                  Align(child: Row(children: [])),
+                ],
+              ),
+            ),
           ),
         ],
       ),
