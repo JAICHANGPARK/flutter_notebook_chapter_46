f8704c1bd4fe79e1af51663733058f5d5d4a2adc
JAICHANGPARK <aristojeff@gmail.com>
2025-08-20 19:09:44 +0900

    Update mental_health_app.dart

--- lib/ep2342_mental_health_app/mental_health_app.dart
+++ lib/ep2342_mental_health_app/mental_health_app.dart
@@ -6,7 +6,7 @@ class MentalHealthApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ,
+      home: MentalHealthHomePage(),
     );
   }
 }
