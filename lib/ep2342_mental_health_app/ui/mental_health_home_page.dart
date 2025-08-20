bd954cf299ec82e97eae977574c3795e5e9ef159
JAICHANGPARK <aristojeff@gmail.com>
2025-08-20 19:09:46 +0900

    Update mental_health_home_page.dart

---
+++ lib/ep2342_mental_health_app/ui/mental_health_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MentalHealthHomePage extends StatefulWidget {
+  const MentalHealthHomePage({super.key});
+
+  @override
+  State<MentalHealthHomePage> createState() => _MentalHealthHomePageState();
+}
+
+class _MentalHealthHomePageState extends State<MentalHealthHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
