fecf78ab5cdd77eab97a16fbd5f9e9f42c77211f
JAICHANGPARK <aristojeff@gmail.com>
2025-08-24 10:19:33 +0900

    Update mental_health_app.dart

--- lib/ep2342_mental_health_app/mental_health_app.dart
+++ lib/ep2342_mental_health_app/mental_health_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'ui/mental_health_article_page.dart';
 import 'ui/mental_health_home_page.dart';
+import 'ui/mental_health_quizzes_page.dart';
 
 class MentalHealthApp extends StatelessWidget {
   const MentalHealthApp({super.key});
