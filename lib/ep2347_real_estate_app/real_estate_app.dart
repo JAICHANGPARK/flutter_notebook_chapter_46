f4432d99ba9692d15c1f887a713b08ec2bcb57fb
JAICHANGPARK <aristojeff@gmail.com>
2025-08-25 17:42:10 +0900

    Update real_estate_app.dart

--- lib/ep2347_real_estate_app/real_estate_app.dart
+++ lib/ep2347_real_estate_app/real_estate_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_46/ep2347_real_estate_app/ui/real_estate_login_page.dart';
 
 
 class RealEstateApp extends StatelessWidget {
@@ -6,6 +7,8 @@ class RealEstateApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: RealEstateLoginPage(),
+    );
   }
 }
