05d6a4ccee3db2a30f2ee2e33aae061dd7bf597e
JAICHANGPARK <aristojeff@gmail.com>
2025-08-25 17:45:13 +0900

    Update real_estate_login_page.dart

--- lib/ep2347_real_estate_app/ui/real_estate_login_page.dart
+++ lib/ep2347_real_estate_app/ui/real_estate_login_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class RealEstateLoginPage extends StatefulWidget {
   const RealEstateLoginPage({super.key});
 
@@ -12,7 +11,16 @@ class _RealEstateLoginPageState extends State<RealEstateLoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Stack(
+        children: [
+          Positioned(
+            child: Image.network(
+              "https://cdn.pixabay.com/photo/2024/03/07/15/57/houses-8618837_1280.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
